71dd96ab131a4003c2a32a45a349d8a4c848ba47
asastreg <alberto_sastre@me.com>
2025-06-21 23:03:57 +0200

    Refactor coordinate transformation functions for consistency and clarity

--- include/coordinates.h
+++ include/coordinates.h
@@ -5,13 +5,31 @@
 #include "data.h"
 
 
-void transform_equatorial_to_eclitical_coord(EquatorialCoord_TypeDef eqCoord, EclipticalCoord_TypeDef* ecCoord);
+/**
+ * @brief Transform equatorial coordinates to ecliptical coordinates
+ *
+ * @param eqCoord Input equatorial coordinates
+ * @param ecCoord Output ecliptical coordinates
+ */
+void transform_equatorial_to_ecliptical_coord(EquatorialCoord_TypeDef eqCoord, EclipticalCoord_TypeDef* ecCoord);
 
+/**
+ * @brief Transform ecliptical coordinates to equatorial coordinates
+ *
+ * @param ecCoord Input ecliptical coordinates
+ * @param eqCoord Output equatorial coordinates
+ */
+void transform_ecliptical_to_equatorial_coord(EclipticalCoord_TypeDef ecCoord, EquatorialCoord_TypeDef* eqCoord);
 
-void transform_eclitical_to_equatorial_coord(EclipticalCoord_TypeDef ecCoord, EquatorialCoord_TypeDef* eqCoord);
-
-
-void transform_equatorial_to_local_horizontal_coord(EquatorialCoord_TypeDef eqCoord, GeodeticCoord_TypeDef geoCoord, Date_TypeDef date, LocalHorizontalCoord_TypeDef* lhCoord);
+/**
+ * @brief Transform equatorial coordinates to local horizontal coordinates
+ *
+ * @param eqCoord Input equatorial coordinates
+ * @param geoCoord Geodetic coordinates of the observer
+ * @param jd Julian day of the observation
+ * @param lhCoord Output local horizontal coordinates
+ */
+void transform_equatorial_to_local_horizontal_coord(EquatorialCoord_TypeDef eqCoord, GeodeticCoord_TypeDef geoCoord, double jd, LocalHorizontalCoord_TypeDef* lhCoord);
 
 
 #endif
--- src/coordinates.c
+++ src/coordinates.c
@@ -2,48 +2,74 @@
 #include "coordinates.h"
 
 #include "time.h"
+#include "utils.h"
 
 #include <math.h>
 
-#include <stdio.h>
-
-void transform_equatorial_to_eclitical_coord(EquatorialCoord_TypeDef eqCoord, EclipticalCoord_TypeDef* ecCoord)
+void transform_equatorial_to_ecliptical_coord(EquatorialCoord_TypeDef eqCoord, EclipticalCoord_TypeDef* ecCoord)
 {
-  double x = cos(eqCoord.right_ascension * 15.0 * deg2rad);
-  double y = sin(eqCoord.right_ascension * 15.0 * deg2rad) * cos(obl_ecliptic_2000 * deg2rad) + tan(eqCoord.declination * deg2rad) * sin(obl_ecliptic_2000 * deg2rad);
+  double lon, lat;
 
-  double sin_B = sin(eqCoord.declination * deg2rad) * cos(obl_ecliptic_2000 * deg2rad) - cos(eqCoord.declination * deg2rad) * sin(obl_ecliptic_2000 * deg2rad) * sin(eqCoord.right_ascension * 15.0 * deg2rad);
+  // Calculate ecliptical longitude (13.1)
+  double y = sin(HOUR_TO_RAD(eqCoord.right_ascension)) * cos(OBL_ECL_2000_RAD) + tan(DEG_TO_RAD(eqCoord.declination)) * sin(OBL_ECL_2000_RAD);
+  double x = cos(HOUR_TO_RAD(eqCoord.right_ascension));
 
-  ecCoord->longitude = atan2(y, x) * rad2deg;
-  ecCoord->latitude = asin(sin_B) * rad2deg;
+  lon = rad_to_360(atan2(y, x));
+
+  // Calculate ecliptical latitude (13.2)
+  double sin_B = sin(DEG_TO_RAD(eqCoord.declination)) * cos(OBL_ECL_2000_RAD) - cos(DEG_TO_RAD(eqCoord.declination)) * sin(OBL_ECL_2000_RAD) * sin(HOUR_TO_RAD(eqCoord.right_ascension));
+
+  lat = rad_to_180(asin(sin_B));
+
+  ecCoord->longitude = lon;
+  ecCoord->latitude = lat;
 }
 
-void transform_eclitical_to_equatorial_coord(EclipticalCoord_TypeDef ecCoord, EquatorialCoord_TypeDef* eqCoord)
+void transform_ecliptical_to_equatorial_coord(EclipticalCoord_TypeDef ecCoord, EquatorialCoord_TypeDef* eqCoord)
 {
-  double x = cos(ecCoord.longitude * deg2rad);
-  double y = sin(ecCoord.longitude * deg2rad) * cos(obl_ecliptic_2000 * deg2rad) - tan(ecCoord.latitude * deg2rad) * sin(obl_ecliptic_2000 * deg2rad);
+  double ra, dec;
 
-  double sin_dec = sin(ecCoord.latitude * deg2rad) * cos(obl_ecliptic_2000 * deg2rad) + cos(ecCoord.latitude * deg2rad) * sin(obl_ecliptic_2000 * deg2rad) * sin(ecCoord.longitude * deg2rad);
+  // Calculate right ascension (13.3)
+  double y = sin(DEG_TO_RAD(ecCoord.longitude)) * cos(OBL_ECL_2000_RAD) - tan(DEG_TO_RAD(ecCoord.latitude)) * sin(OBL_ECL_2000_RAD);
+  double x = cos(DEG_TO_RAD(ecCoord.longitude));
 
-  eqCoord->right_ascension = atan2(y, x) * rad2deg / 15.0;
-  eqCoord->declination = asin(sin_dec) * rad2deg;
+  ra = RAD_TO_HOUR(rad_to_2pi(atan2(y, x)));
+
+  // Calculate declination (13.4)
+  double sin_dec = sin(DEG_TO_RAD(ecCoord.latitude)) * cos(OBL_ECL_2000_RAD) + cos(DEG_TO_RAD(ecCoord.latitude)) * sin(OBL_ECL_2000_RAD) * sin(DEG_TO_RAD(ecCoord.longitude));
+
+  dec = rad_to_180(asin(sin_dec));
+
+  eqCoord->right_ascension = ra;
+  eqCoord->declination = dec;
 }
 
-void transform_equatorial_to_local_horizontal_coord(EquatorialCoord_TypeDef eqCoord, GeodeticCoord_TypeDef geoCoord, Date_TypeDef date, LocalHorizontalCoord_TypeDef* lhCoord)
+void transform_equatorial_to_local_horizontal_coord(EquatorialCoord_TypeDef eqCoord, GeodeticCoord_TypeDef geoCoord, double jd, LocalHorizontalCoord_TypeDef* lhCoord)
 {
+  double az, al;
+
+  // Calculate Greenwich Mean Sidereal Time (GMST) from Julian Day
   Time_TypeDef gmst;
-  get_greenwich_mean_sideral_time(date, &gmst);
+  get_greenwich_mean_sideral_time_from_jd(jd, &gmst);
 
+  // Convert GMST to decimal degrees
   double gmst_deg;
-  get_decimal_degrees_from_time(gmst, &gmst_deg);
+  convert_time_to_decimal_degrees(gmst, &gmst_deg);
 
-  double local_hour_angle = fmod(fmod(gmst_deg + geoCoord.longitude - eqCoord.right_ascension * 15.0, 360) + 360, 360);
+  // Calculate Local Hour Angle (LHA) in radians
+  double local_hour_angle = deg_to_2pi(gmst_deg - geoCoord.longitude - HOUR_TO_DEG(eqCoord.right_ascension));
 
-  double x = cos(local_hour_angle * deg2rad) * sin(geoCoord.latitude * deg2rad) - tan(eqCoord.declination * deg2rad) * cos(geoCoord.latitude * deg2rad);
-  double y = sin(local_hour_angle * deg2rad);
+  // Calculate Azimuth (13.5)
+  double y = sin(local_hour_angle);
+  double x = cos(local_hour_angle) * sin(DEG_TO_RAD(geoCoord.latitude)) - tan(DEG_TO_RAD(eqCoord.declination)) * cos(DEG_TO_RAD(geoCoord.latitude));
 
-  double sin_h = sin(geoCoord.latitude * deg2rad) * sin(eqCoord.declination * deg2rad) + cos(geoCoord.latitude * deg2rad) * cos(eqCoord.declination * deg2rad) * cos(local_hour_angle * deg2rad);
+  az = rad_to_360(atan2(y, x));
 
-  lhCoord->azimuth = atan2(y, x) * rad2deg;
-  lhCoord->altitude = asin(sin_h) * rad2deg;
+  // Calculate Altitude (13.6)
+  double sin_h = sin(DEG_TO_RAD(geoCoord.latitude)) * sin(DEG_TO_RAD(eqCoord.declination)) + cos(DEG_TO_RAD(geoCoord.latitude)) * cos(DEG_TO_RAD(eqCoord.declination)) * cos(local_hour_angle);
+
+  al = rad_to_180(asin(sin_h));
+
+  lhCoord->azimuth = az;
+  lhCoord->altitude = al;
 }
